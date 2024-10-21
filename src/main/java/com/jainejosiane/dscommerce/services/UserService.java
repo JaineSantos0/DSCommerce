6bc1969e2b0020f69e536fae1b9e2330b6607fa3
JaineSantos0 <jainejosiane@gmail.com>
2024-10-21 14:22:16 -0300

    feat: adding AuthorizationServer and ResourceServer not tested

--- src/main/java/com/jainejosiane/dscommerce/services/UserService.java
+++ src/main/java/com/jainejosiane/dscommerce/services/UserService.java
@@ -1,5 +1,6 @@
 package com.jainejosiane.dscommerce.services;
 
+import com.jainejosiane.dscommerce.entities.Role;
 import com.jainejosiane.dscommerce.entities.User;
 import com.jainejosiane.dscommerce.projections.UserDetailsProjection;
 import com.jainejosiane.dscommerce.repositories.UserRepository;
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService implements UserDetailsService {
 
     @Autowired
@@ -29,5 +32,4 @@ public class UserService implements UserDetailsService {
         }
         return user;
     }
-
 }
