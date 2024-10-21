6da84b02081b17113c610e0e2038db39a53b9dc6
JaineSantos0 <jainejosiane@gmail.com>
2024-10-21 13:45:32 -0300

    feat: adding Spring Security not tested

---
+++ src/main/java/com/jainejosiane/dscommerce/services/UserService.java
@@ -0,0 +1,33 @@
+package com.jainejosiane.dscommerce.services;
+
+import com.jainejosiane.dscommerce.entities.User;
+import com.jainejosiane.dscommerce.projections.UserDetailsProjection;
+import com.jainejosiane.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+        return user;
+    }
+
+}
