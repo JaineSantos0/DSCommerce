f4b89378fc7ebd124ee479333e5f8da2a9fea9df
JaineSantos0 <jainejosiane@gmail.com>
2024-10-25 13:52:22 -0300

    feat: adding get all categories

---
+++ src/main/java/com/jainejosiane/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.jainejosiane.dscommerce.controllers;
+
+import com.jainejosiane.dscommerce.dto.CategoryDTO;
+import com.jainejosiane.dscommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
